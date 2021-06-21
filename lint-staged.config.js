335a999c9330087902064116690df47d66fa49fd
vitorserrano <vitorcirinoserrano@gmail.com>
2021-06-21 19:45:18 -0300

    fix: fixed husky pre-commit

--- lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // Run type-check on changes to TypeScript files
-  "**/*.ts?(x)": () => "yarn typecheck",
-  // Run ESLint on changes to JavaScript/TypeScript files
-  "**/*.(ts|js)?(x)": (filenames) => `yarn lint ${filenames.join(" ")}`,
-};
