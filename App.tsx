f3d90009e5e5b5750f4b0d6828bf37469ceb807e
vitorserrano <vitorcirinoserrano@gmail.com>
2021-06-21 21:27:17 -0300

    general update

--- App.tsx
+++ App.tsx
@@ -1,21 +1,14 @@
 import React from 'react'
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
 
-export const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  )
-}
+import { ThemeProvider } from 'styled-components'
+import { appTheme } from './src/styles/theme'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
+import { SignIn } from './src/pages/SignIn'
+
+const App = () => (
+  <ThemeProvider theme={appTheme}>
+    <SignIn />
+  </ThemeProvider>
+)
+
+export default App
